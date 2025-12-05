24ae73c918ebecc7b43d5713d2d1212e158d170d
DESKTOP-2S63EIQ\student <>
2025-12-05 15:09:11 +0900

    no message

---
+++ Assets/ItemBase.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+
+	// 表示等をするためにGameObjectをSeriaLizeField
+	[SerializeField] GameObject GameObject;
+	//初期化処理
+	public virtual void Start()
+	{
+		// アイテムの初期位置の設定
+		// アイテムの表示の設定
+		m_IsVisible = true;
+
+		// アイテムがアクティブかの設定
+		m_IsActive = false;
+
+		// アイテムのタグの設定 
+	
+	}
+
+	// Update is called once per frame
+	public virtual void Update()
+	{
+		GameObject.SetActive(m_IsVisible);
+	}
+	// アイテムの座標情報
+	public Vector3 m_Position = new Vector3(0f, 0f, 0f);
+
+	// アイテムのタグ
+	public string m_ItemTag = "None";
+
+	// アイテムがアクティブかどうか
+	public bool m_IsActive = false;
+
+	// アイテムを表示されているか
+	public bool m_IsVisible = true;
+
+	// プレイヤーがアイテムをとれるかどうか
+	public bool IsGetItem() { return m_IsActive; }
+
+	// アイテムがアクティブかどうかの情報を設定
+	public void SetActive(bool _IsActive) { m_IsActive = _IsActive; }
+
+	// プレイヤーがアイテムをとった時の処理
+	public virtual void GetItem()
+	{
+		// アイテムがアクティブであれば処理
+		if(m_IsActive)
+		{
+			// アイテムを取得した時のサウンドを鳴らす
+			// まだない 12/3
+
+			// アイテムを非表示
+			m_IsVisible = false;
+			// 非アクティブにする
+			m_IsActive = false;
+		}
+	}
+}
---
+++ Assets/playerMove.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEmptyCSharpScript:MonoBehaviour
+{
+    //キャラクタコントローラーを使う為の変数
+    public CharacterController characterController;
+    //動く速さ
+    public float walkSpeed;
+    public float slowwalkSpeed;
+    public float runSpeed;
+
+    //マウスの感度
+    public float mauseSensitivti;
+    //kamera
+    public Transform cam;
+
+    private float xRotation;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical)*Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movement = transform.rotation*movement*runSpeed;
+        }
+        else if(Input.GetKey(KeyCode.Tab))
+        {
+            movement = transform.rotation * movement * slowwalkSpeed;
+        }
+        else
+        {
+            movement = transform.rotation * movement * walkSpeed;
+        }
+
+        characterController.Move(movement);
+
+
+
+        //カメラの動き
+        //X
+        float mauseX = Input.GetAxisRaw("Mouse X") * mauseSensitivti*Time.deltaTime;
+        transform.Rotate(Vector3.up * mauseX);
+        //Y
+        float mouseY= Input.GetAxisRaw("Mouse Y") * mauseSensitivti * Time.deltaTime;
+        xRotation -= mouseY;
+        //振り向き制限
+        xRotation = Mathf.Clamp(xRotation, -60.0f, 60.0f);
+        cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+    }
+}
